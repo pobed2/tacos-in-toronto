a6ed8c699739fe94367b6255b18344e63d19879c
Pierre-Olivier Bedard <pobed2@gmail.com>
2015-06-29 15:54:04 -0400

    Fixed typo

--- config/env/production.js
+++ config/env/production.js
@@ -11,7 +11,7 @@
  */
 
 module.exports = {
-  hookTimeout: 60000
+  hookTimeout: 60000,
 
   /***************************************************************************
    * Set the default database connection for models in the production        *
@@ -32,7 +32,7 @@ module.exports = {
    * Set the port in the production environment to 80                        *
    ***************************************************************************/
 
-  port: 80,
+  port: 80
 
   /***************************************************************************
    * Set the log level in production environment to "silent"                 *
