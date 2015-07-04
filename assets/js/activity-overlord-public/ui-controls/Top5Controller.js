0fe23b3fa3de71be7c8bb1e409697c4911ffe74a
Pierre-Olivier Bedard <pobed2@gmail.com>
2015-07-04 15:05:52 -0400

    Added top-5.
    Added top 5 page.
    I think I scrapped the login pages…

---
+++ assets/js/activity-overlord-public/ui-controls/Top5Controller.js
@@ -0,0 +1,59 @@
+angular.module('ActivityOverlordPublic', ['ui.grid']).controller('Top5Controller', ['$scope','$http', function($scope, $http) {
+  var top = this;
+  /////////////////////////////////////////////////////////////////////////////////
+  // When HTML is rendered...
+  /////////////////////////////////////////////////////////////////////////////////
+
+  top.keywords =[];
+  loadTopFive();
+
+  /////////////////////////////////////////////////////////////////////////////////
+  // DOM events:
+  /////////////////////////////////////////////////////////////////////////////////
+  io.socket.on('keyword', function (event){
+    if (event.verb === 'updated') {
+      var foundKeyword = _.find(top.keywords, {id: event.id});
+      if (foundKeyword && (event.data.times === 'undefined' || event.dataevent.data.times >= foundKeyword.times)) {
+        _.extend(foundKeyword, event.data);
+        $scope.$apply();
+      }else{
+        top.loadTopFive();
+      }
+
+      return;
+    }
+
+    if (event.verb === 'created') {
+      //A new keyword necessitates a full refresh of the Top5
+      top.loadTopFive()
+      return;
+    }
+
+    if (event.verb === 'destroyed') {
+      //A full refresh from DB is only necessary when keyword is in top 5
+      var foundKeyword = _.find(top.keywords, {id: event.id});
+      if (foundKeyword) {
+        loadTopFive();
+      }
+      return;
+    }
+
+    throw new Error('Unexpected/unknown socket event: "'+event.verb+'" received from Sails.');
+  });
+
+
+  /////////////////////////////////////////////////////////////////////////////////
+  // Private methods:
+  /////////////////////////////////////////////////////////////////////////////////
+   function loadTopFive(){
+    io.socket.get("/keyword/topfive", function onResponse(data, jwr){
+    if (jwr.error) {
+      console.log(jwr.error);
+      return;
+    }
+    top.keywords = data;
+    $scope.$apply();
+  })};
+  top.loadTopFive = loadTopFive;
+
+}]);
